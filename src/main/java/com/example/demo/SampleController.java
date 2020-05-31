bc7d52a311d09616598b1fc61aa876446fc7136e
hanum20 <woom2290@gmail.com>
2020-05-31 14:29:16 +0900

    20200531 study

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -2,44 +2,80 @@ package com.example.demo;
 
         import org.springframework.http.HttpHeaders;
         import org.springframework.http.MediaType;
+        import org.springframework.http.ResponseEntity;
         import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.validation.BindingResult;
+        import org.springframework.validation.annotation.Validated;
         import org.springframework.web.bind.annotation.*;
+        import org.springframework.web.bind.support.SessionStatus;
+        import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-        import java.awt.*;
+        import javax.servlet.http.HttpSession;
+        import javax.validation.Valid;
+        import java.time.LocalDateTime;
+        import java.util.ArrayList;
+        import java.util.List;
 
 @Controller
+@SessionAttributes("event")
 public class SampleController {
 
-    @GetHelloMapping
-    @ResponseBody
-    public String hello(){
-        return "hello";
+    @GetMapping("/events/form/name")
+    public String eventsFormName(Model model){
+        model.addAttribute("event", new Event());
+        return "/events/form-name";
+    }
+    //session의 값도 바인딩을 받음
+    @PostMapping("/events/form/name")
+    public String createFormNameSubmit(@Validated @ModelAttribute Event event,
+                              BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "/events/form-name";
+        }
+
+        return "redirect:/events/form/limit";
     }
 
-    @GetMapping("/events")
-    @ResponseBody
-    public String events(){
-        return "events";
+    @GetMapping("/events/form/limit")
+    public String eventsFormLimit(@ModelAttribute Event event, Model model){
+       // model.addAttribute("event", event);
+        return "/events/form-limit";
     }
 
-    @GetMapping("/event/{id}")
-    @ResponseBody
-    public String event(@PathVariable int id){
-        return "event";
+    //session의 값도 바인딩을 받음
+    @PostMapping("/events/form/limit")
+    public String createFormLimitSubmit(@Validated @ModelAttribute Event event,
+                                        BindingResult bindingResult,
+                                        SessionStatus sessionStatus,
+                                        RedirectAttributes attributes){
+        if(bindingResult.hasErrors()){
+            return "/events/form-limit";
+        }
+
+        sessionStatus.setComplete();
+        attributes.addAttribute("name", event.getName());
+        attributes.addAttribute("limit", event.getLimit());
+        return "redirect:/events/list";
     }
 
-    @PostMapping(
-            value = "/events",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public String createEvent(){
-        return "createEvent";
-    }
+    @GetMapping("/events/list")
+    public String getEvents(@RequestParam String name,
+                            @RequestParam Integer limit,
+                            Model model,
+                            HttpSession httpSession) {
+        LocalDateTime visitTime = (LocalDateTime) httpSession.getAttribute("visitTime");
+        System.out.println(visitTime);
 
-    @DeleteMapping("/events")
-    @ResponseBody
-    public String deleteEvent(){
-        return "deleteEvent";
+        Event newEvent = new Event();
+        newEvent.setName(name);
+        newEvent.setLimit(limit);
+
+        List<Event> eventList = new ArrayList<>();
+        eventList.add(newEvent);
+
+        model.addAttribute(eventList);
+
+        return "/events/list";
     }
 }
--- src/test/java/com/example/demo/SampleControllerTest.java
+++ src/test/java/com/example/demo/SampleControllerTest.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.ModelAndView;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringRunner.class)
@@ -25,9 +29,24 @@ public class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void hello() throws Exception {
-        mockMvc.perform(head("/hello"))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk());
+    public void getEvent() throws Exception {
+        ResultActions result = mockMvc.perform(post("/events")
+                    .param("name","hanum")
+                    .param("limit","-10"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(model().hasErrors());
+    }
+
+    @Test
+    public void eventsForm() throws Exception {
+         MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
+                .andDo(print())
+                .andExpect(view().name("/events/form"))
+                .andExpect(model().attributeExists("event"))
+                .andExpect(request().sessionAttribute("event", notNullValue()))
+                .andReturn().getRequest();
+         Object event = request.getSession().getAttribute("event");
+         System.out.println(event);
     }
 }
